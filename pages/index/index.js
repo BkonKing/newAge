98a35bdd230ea83c4eda150fff710e3c743fde4e
hongbingkun <hongbingkun@bosssoft.com.cn>
2019-11-29 10:16:43 +0800

    测试3

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,6 +18,7 @@ Page({
   onLoad: function () {
     console.log(2222)
     console.log(3333)
+    console.log(4444)
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
